b56664d7de36b83722fb578754e6515f7622d672
jplanaux <55534166+jplanaux@users.noreply.github.com>
2019-09-29 02:41:59 -0500

    Create picture.php

---
+++ www/picture.php
@@ -0,0 +1,20 @@
+<?php
+shell_exec("/usr/bin/sudo /var/www/html/camera.sh");
+?>
+<html>
+<body>
+<title>Take Picture</title>
+<style type=text/css>
+h1{
+	padding-left: 300px;
+}
+h2{
+	position: absolute;
+	top: 100px;
+	left: 450px;
+}
+</style>
+<h1>Welcome to Gatsby, Midnight, and Gaston's web server</h2>
+<h2>I have just taken a current photo. Press the back button and refresh to see the new image</h2>
+</body>
+</html>
