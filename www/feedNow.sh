92f0055a35e1063a553035974615cc5335ec9712
jplanaux <55534166+jplanaux@users.noreply.github.com>
2019-09-29 02:39:23 -0500

    Create feedNow.sh

---
+++ www/feedNow.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# feedNow.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/icf/catFeeder
+sudo python feedNow.py
+cd /
