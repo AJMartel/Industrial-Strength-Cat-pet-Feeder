620b9d0ff1d4b9e25fda15b393ce52ddffdf75b3
jplanaux <55534166+jplanaux@users.noreply.github.com>
2019-10-25 02:27:53 -0500

    Update sendBeforeEmail.py

--- icf/catFeeder/sendBeforeEmail.py
+++ icf/catFeeder/sendBeforeEmail.py
@@ -1,7 +1,27 @@
-import time
-import urllib2
-import shutil
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
 
-from subprocess import call
-# replace name@provider.com with your email address
-call(["mpack", "-s", "Before Feed", "/home/icf/beforeImage.jpg", "name@provider.com"])
+fromaddr = "name@provider.com"
+toaddr = "reciepient@provider.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Before Feed"
+body = "Before Feeding"
+msg.attach(MIMEText(body, 'plain'))
+filename = "beforeImage.jpg"
+attachment = open("/home/icf/beforeImage.jpg", "rb")
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+msg.attach(part)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("userName", "password")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
